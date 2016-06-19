995963f3dbe20cb33c402a0a8856d39c65e56031
Josh Jarmain <jjarmain@gmail.com>
2016-06-19 11:33:03 -0700

    working todo but needs validation

---
+++ src/app.jsx
@@ -0,0 +1,30 @@
+require('../node_modules/todomvc-app-css/index.css');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {List, Map} from 'immutable';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducers/reducer';
+import {TodoAppContainer} from './components/TodoApp';
+
+const store = createStore(reducer);
+
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    todos: [
+      {id: 1, text: 'React', status: 'active', editing: false},
+      {id: 2, text: 'Redux', status: 'active', editing: false},
+      {id: 3, text: 'Immutable', status: 'active', editing: false},
+    ],
+    filter: 'all'
+  }
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoAppContainer />
+  </Provider>,
+  document.getElementById('app')
+);
